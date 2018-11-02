dff963ab5baef2f0030bded50b3a34bb6b0ae24b
Ch3Tu <ConCh3TuXD@gmail.com>
2018-11-01 23:35:47 -0500

    second commint proyect

---
+++ src/main/java/com/cozy/admin/Outs.java
@@ -0,0 +1,47 @@
+package com.cozy.admin;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebServlet("/old")
+public class Outs extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+ 
+		String message = req.getParameter("message");
+		resp.setContentType("application/json");
+		System.out.println("atributo desde el filtro " + req.getAttribute("segundoEstado") );
+		try (PrintWriter out = resp.getWriter()){
+			out.print("{\"message\": \"" + message + "\"}");
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String line;
+		StringBuffer json = new StringBuffer();
+		try (BufferedReader reader = req.getReader()){
+			while ((line = reader.readLine()) != null){
+				json.append(line);
+			}
+		}
+		System.out.println(json.toString());
+		ObjectMapper mapper = new ObjectMapper();
+		Message message = mapper.readValue(json.toString(), Message.class);
+		System.out.println("message size" + message.getSize());
+	}
+
+	
+	
+	
+}
